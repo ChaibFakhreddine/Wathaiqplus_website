7acbbb6691f685347a9b90c49461344298c04b2e
Fakhreddine Chaib <fakhreddinechaib@gmail.com>
2025-07-05 14:37:19 +0100

    Fix DATABASE_URL typo in .env

--- project/settings.py
+++ project/settings.py
@@ -76,11 +76,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'project/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'project/static')
-]
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
